2f89ec01a3e183015fd14328480f63b434582fb4
ZacharyDflores <debianx11@gmail.com>
2017-02-13 21:03:23 -0400

    Update adicionarOs.php
    Traducción al español de Venezuela y añadido botón para agregar clientes en el formulario de ordenes de servicio

--- application/views/os/adicionarOs.php
+++ application/views/os/adicionarOs.php
@@ -8,21 +8,21 @@
                 <span class="icon">
                     <i class="icon-tags"></i>
                 </span>
-                <h5>Cadastro de OS</h5>
+                <h5>Registrar O.S</h5>
             </div>
             <div class="widget-content nopadding">
                 
 
                 <div class="span12" id="divProdutosServicos" style=" margin-left: 0">
                     <ul class="nav nav-tabs">
-                        <li class="active" id="tabDetalhes"><a href="#tab1" data-toggle="tab">Detalhes da OS</a></li>
+                        <li class="active" id="tabDetalhes"><a href="#tab1" data-toggle="tab">Detalles de la O.S</a></li>
                     </ul>
                     <div class="tab-content">
                         <div class="tab-pane active" id="tab1">
 
                             <div class="span12" id="divCadastrarOs">
                                 <?php if($custom_error == true){ ?>
-                                <div class="span12 alert alert-danger" id="divInfo" style="padding: 1%;">Dados incompletos, verifique os campos com asterisco ou se selecionou corretamente cliente e responsável.</div>
+                                <div class="span12 alert alert-danger" id="divInfo" style="padding: 1%;">Datos incompletos, comprobar los campos con asterisco o cliente correctamente seleccionado y responsable.</div>
                                 <?php } ?>
                                 <form action="<?php echo current_url(); ?>" method="post" id="formOs">
 
@@ -31,35 +31,38 @@
                                             <label for="cliente">Cliente<span class="required">*</span></label>
                                             <input id="cliente" class="span12" type="text" name="cliente" value=""  />
                                             <input id="clientes_id" class="span12" type="hidden" name="clientes_id" value=""  />
+                                        <div class="span6">
+                                        <a href="#modalCliente" data-toggle="modal" role="button" class="btn btn-success tip-bottom" title="Registrar Nuevo Cliente"><i class="icon-plus icon-white"></i> Nuevo Cliente</a>  
+                                        </div>
                                         </div>
                                         <div class="span6">
-                                            <label for="tecnico">Técnico / Responsável<span class="required">*</span></label>
+                                            <label for="tecnico">Técnico / Responsble<span class="required">*</span></label>
                                             <input id="tecnico" class="span12" type="text" name="tecnico" value=""  />
                                             <input id="usuarios_id" class="span12" type="hidden" name="usuarios_id" value=""  />
                                         </div>
                                     </div>
                                     <div class="span12" style="padding: 1%; margin-left: 0">
                                         <div class="span3">
-                                            <label for="status">Status<span class="required">*</span></label>
-                                            <select class="span12" name="status" id="status" value="">
-                                                <option value="Orçamento">Orçamento</option>
-                                                <option value="Aberto">Aberto</option>
-                                                <option value="Em Andamento">Em Andamento</option>
+                                            <label for="status">Estado<span class="required">*</span></label>
+                                           <select class="span12" name="status" id="status" value="">
+                                                <option value="Presupuesto">Presupuestado</option>
+                                                <option value="Abierto">Abierto</option>
+                                                <option value="En Curso">En Curso</option>
                                                 <option value="Finalizado">Finalizado</option>
                                                 <option value="Cancelado">Cancelado</option>
                                             </select>
                                         </div>
                                         <div class="span3">
-                                            <label for="dataInicial">Data Inicial<span class="required">*</span></label>
-                                            <input id="dataInicial" class="span12 datepicker" type="text" name="dataInicial" value="<?php echo date('d/m/Y'); ?>"  />
+                                            <label for="dataInicial">Fecha Inicial<span class="required">*</span></label>
+                                            <input id="dataInicial" class="span12 datepicker" type="text" name="dataInicial" value=""  />
                                         </div>
                                         <div class="span3">
-                                            <label for="dataFinal">Data Final</label>
+                                            <label for="dataFinal">Fecha Final</label>
                                             <input id="dataFinal" class="span12 datepicker" type="text" name="dataFinal" value=""  />
                                         </div>
 
                                         <div class="span3">
-                                            <label for="garantia">Garantia</label>
+                                            <label for="garantia">Garantía</label>
                                             <input id="garantia" type="text" class="span12" name="garantia" value=""  />
                                         </div>
                                     </div>
@@ -67,29 +70,30 @@
                                     <div class="span12" style="padding: 1%; margin-left: 0">
 
                                         <div class="span6">
-                                            <label for="descricaoProduto">Descrição Produto/Serviço</label>
+                                            <label for="descricaoProduto">Descripción Produto/Servicio</label>
                                             <textarea class="span12" name="descricaoProduto" id="descricaoProduto" cols="30" rows="5"></textarea>
+                                                                                  
                                         </div>
                                         <div class="span6">
-                                            <label for="defeito">Defeito</label>
+                                            <label for="defeito">Defecto</label>
                                             <textarea class="span12" name="defeito" id="defeito" cols="30" rows="5"></textarea>
                                         </div>
 
                                     </div>
                                     <div class="span12" style="padding: 1%; margin-left: 0">
                                         <div class="span6">
-                                            <label for="observacoes">Observações</label>
+                                            <label for="observacoes">Observaciones</label>
                                             <textarea class="span12" name="observacoes" id="observacoes" cols="30" rows="5"></textarea>
                                         </div>
                                         <div class="span6">
-                                            <label for="laudoTecnico">Laudo Técnico</label>
+                                            <label for="laudoTecnico">Diagnóstico Técnico</label>
                                             <textarea class="span12" name="laudoTecnico" id="laudoTecnico" cols="30" rows="5"></textarea>
                                         </div>
                                     </div>
                                     <div class="span12" style="padding: 1%; margin-left: 0">
                                         <div class="span6 offset3" style="text-align: center">
                                             <button class="btn btn-success" id="btnContinuar"><i class="icon-share-alt icon-white"></i> Continuar</button>
-                                            <a href="<?php echo base_url() ?>index.php/os" class="btn"><i class="icon-arrow-left"></i> Voltar</a>
+                                            <a href="<?php echo base_url() ?>index.php/os" class="btn"><i class="icon-arrow-left"></i> Volver</a>
                                         </div>
                                     </div>
                                 </form>
@@ -110,6 +114,102 @@
 </div>
 </div>
 
+
+<!-- Modal Cliente-->
+
+<div id="modalCliente" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <form id="formClientes" action="<?php echo base_url() ?>index.php/clientes/adicionar" method="post">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">RMA O.S - Agregar Cliente</h3>
+  </div>
+  <div class="modal-body">
+    <div class="widget-content nopadding">
+                <?php if ($custom_error != '') {
+                    echo '<div class="alert alert-danger">' . $custom_error . '</div>';
+                } ?>
+                     <form action="<?php echo current_url(); ?>" id="formCliente" method="post" class="form-horizontal" >
+                    <div class="control-group">
+                        <label for="nomeCliente" class="control-label">Nombre<span class="required">*</span></label>
+                        <div class="controls">
+                            <input id="nomeCliente" type="text" name="nomeCliente" value="<?php echo set_value('nomeCliente'); ?>"  />
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label for="documento" class="control-label">Cedula/DNI<span class="required">*</span></label>
+                        <div class="controls">
+                            <input id="documento" type="text" name="documento" value="<?php echo set_value('documento'); ?>"  />
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label for="telefone" class="control-label">Teléfono<span class="required">*</span></label>
+                        <div class="controls">
+                            <input id="telefone" type="text" name="telefone" value="<?php echo set_value('telefone'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label for="celular" class="control-label">Tel. Móvil</label>
+                        <div class="controls">
+                            <input id="celular" type="text" name="celular" value="<?php echo set_value('celular'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label for="email" class="control-label">Email</label>
+                        <div class="controls">
+                            <input id="email" type="text" name="email" value="<?php echo set_value('email'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group" class="control-label">
+                        <label for="rua" class="control-label">Dirección</label>
+                        <div class="controls">
+                            <input id="rua" type="text" name="rua" value="<?php echo set_value('rua'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label for="numero" class="control-label">Número</label>
+                        <div class="controls">
+                            <input id="numero" type="text" name="numero" value="<?php echo set_value('numero'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group" class="control-label">
+                        <label for="bairro" class="control-label">Barrio</label>
+                        <div class="controls">
+                            <input id="bairro" type="text" name="bairro" value="<?php echo set_value('bairro'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group" class="control-label">
+                        <label for="cidade" class="control-label">Ciudad</label>
+                        <div class="controls">
+                            <input id="cidade" type="text" name="cidade" value="<?php echo set_value('cidade'); ?>"  />
+                        </div>
+                    </div>
+
+                    <div class="control-group" class="control-label">
+                        <label for="estado" class="control-label">País</label>
+                        <div class="controls">
+                            <input id="estado" type="text" name="estado" value="<?php echo set_value('estado'); ?>"  />
+                        </div>
+                    </div>
+                   
+                    <div class="form-actions">
+                        <div class="span12">
+                            <div class="span6 offset3">
+                                <button type="submit" class="btn btn-success"><i class="icon-plus icon-white"></i> Agregar</button>
+                                <a href="<?php echo base_url() ?>index.php/os/adicionar" id="" class="btn"><i class="icon-arrow-left"></i> Volver</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <script type="text/javascript">
@@ -168,4 +268,3 @@ $(document).ready(function(){
 });
 
 </script>
-
